8ec42a782fa56aefa36253c5b0dd7ad8d42efd0f
Midfield <45480939+MidfieId@users.noreply.github.com>
2025-04-22 18:21:19 +0400

    finalized game functions

--- script.js
+++ script.js
@@ -8,20 +8,41 @@ function getComputerChoice(){
         return 'scissors';
     }
 }
-let score = 0;
-console.log(getComputerChoice());
 
 function getHumanChoice(){
     let humanChoice = prompt('Enter your choice: ').toLowerCase();
     return humanChoice;
 }
 
-function playRound(){
+
+function playGame(){ 
     let humanScore = 0;
     let computerScore = 0;
-    let computerChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
-    if(humanChoice == computerChoice){
-        console.log(`It's a draw, Score line is \n ${humanChoice} \n ${computerChoice}`)
+
+    function playRound(){
+        let computerChoice = getComputerChoice();
+        let humanChoice = getHumanChoice();
+        if(humanChoice === computerChoice){
+            console.log(`It's a draw, Score line is \n Human: ${humanScore} \n Computer: ${computerScore}`)
+        }
+        if((humanChoice === 'rock' && computerChoice ==='scissors') || (humanChoice === 'scissors' && computerChoice ==='paper') || (humanChoice === 'paper' && computerChoice ==='rock')){
+            humanScore++;
+            console.log(`You win!, Score line is \n Human: ${humanScore} \n Computer: ${computerScore}`)
+        }
+        if((computerChoice === 'rock' && humanChoice ==='scissors') || (computerChoice === 'scissors' && humanChoice ==='paper') || (computerChoice === 'paper' && humanChoice ==='rock')){
+            computerScore++;
+            console.log(`Computer wins!, Score line is \n Human: ${humanScore} \n Computer: ${computerScore}`)
+        }
+    }
+    
+    for(let i = 0;i<5;i++){
+        playRound();
+    }
+    if(humanScore > computerScore){
+        console.log('Game Over, You won!')
+    } else {
+        console.log('Game Over, You Lose')
     }
 }
+
+playGame();
